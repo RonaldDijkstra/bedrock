30893a711f133dc75340c4e5c3ddcaaeb5645793
Ronald Dijkstra <ronalddijkstra.nl@gmail.com>
2023-06-26 20:59:41 +0200

    Install bedrock and sage

---
+++ web/app/themes/folkingebrew/resources/views/sections/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-black transition-all duration-150 ease-in-out w-full z-10 motion-reduce:transition-none">
+  <a class="brand text-white" href="{{ home_url('/') }}">
+    {!! $siteName !!}
+  </a>
+
+  @if (has_nav_menu('primary_navigation'))
+    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+    </nav>
+  @endif
+</header>
